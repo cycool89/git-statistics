f2df07cf3caa4fc5c6e01ef929f11eea6226d77c
Kigyós János <cycool89@gmail.com>
2021-03-28 16:22:18 +0200

    Commits per user stat groups by email

--- src/functions/commits-per-user.sh
+++ src/functions/commits-per-user.sh
@@ -5,6 +5,17 @@ function commits_per_user () {
         log "Commits per user"
     else
         log "Commits per user without merge"
-    fi    
-    git shortlog -sne $1
+    fi
+    RESULT=`git shortlog -sne $1 | sed -E 's/([0-9]+).*<(.*)>/\2\t\1/g'`
+    declare -A stats
+    while IFS= read -r line ; do 
+        userstats=($line)
+        if [ "${stats[${userstats[0]}]}" == "" ]; then
+            stats[${userstats[0]}]=0
+        fi
+        stats[${userstats[0]}]=$(( ${stats[${userstats[0]}]} + ${userstats[1]} ))
+    done <<< $RESULT
+
+# TODO rendezni a stats tömböt...
+    for K in "${!stats[@]}"; do echo $K --- ${stats[$K]}; done
 }
